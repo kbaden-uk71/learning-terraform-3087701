4f8690077bf84be5aaa3e06ba2a111936bfd80cc
kbaden-uk71 <133954864+kbaden-uk71@users.noreply.github.com>
2023-05-20 13:21:30 +0100

    create dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+/*
 data "aws_vpc" "default" {
   default = true
+}
+*/
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  
+
+  tags = {
+    Terraform = "true" 
+    Environment = "dev"
+  }
 }
 
 
@@ -35,7 +55,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
   name    = "blog_new"
-  vpc_id  = data.aws_vpc.default.id
+  
+  #vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.vpc.public_subnets[0]
   
   ingress_rules       = [ "http-80-tcp", "https-443-tcp" ]
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
@@ -64,7 +86,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
-  from_port   = 443
+   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = [ "0.0.0.0/0" ]
