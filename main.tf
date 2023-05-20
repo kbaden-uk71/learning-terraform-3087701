eaeb53e5ace386f1be4cc2fa82f103cf48eb7944
kbaden-uk71 <133954864+kbaden-uk71@users.noreply.github.com>
2023-05-20 14:21:28 +0100

    load blancer added

--- main.tf
+++ main.tf
@@ -52,6 +52,48 @@ resource "aws_instance" "blog" {
   }
 }
 
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        } 
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
@@ -66,42 +108,3 @@ module "blog_sg" {
   egress_rules       = [ "all-all" ]
   egress_cidr_blocks = [ "0.0.0.0/0" ]
 }
-
-/*
-resource "aws_security_group" "blog" {
-  name        = "secgrp_blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-  }
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = [ "0.0.0.0/0" ]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-   from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = [ "0.0.0.0/0" ]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = [ "0.0.0.0/0" ]
-
-  security_group_id = aws_security_group.blog.id
-}
-*/
