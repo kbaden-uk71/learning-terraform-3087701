bc4102414759fad54a34222dd9e98df2a2a352be
kbaden-uk71 <133954864+kbaden-uk71@users.noreply.github.com>
2023-05-20 14:09:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,9 +27,10 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false # was true, route id error
+  enable_nat_gateway = true # was true, route id error
   
 
   tags = {
